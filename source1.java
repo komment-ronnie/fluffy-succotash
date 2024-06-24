fa61b9b9a223a06d5c2965d75cd3d4fdb51465f1
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-06-24 10:17:53 +0100

    Update source1.java

--- source1.java
+++ source1.java
@@ -15,7 +15,6 @@ public class PageInfo {
   private final int pageLimit;
   private final int totalPages;
   private final long totalElements;
-
     public static PageInfo of(Pageable pageable, Page<?> page) {
     return new PageInfo(
         pageable.getPageNumber(),
