9ce3197dd51a712917ebbf64d84e9776f4ad5fe0
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-06-24 09:24:13 +0100

    Update source.java

--- source.java
+++ source.java
@@ -6,11 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-/**
- * Is a summary of page numbers, page sizes, total pages, and total elements in a
- * paginated result. The class provides information on the number of pages and size
- * of each page, as well as the total number of pages and elements in the overall result.
- */
+
 @EqualsAndHashCode
 @ToString
 @Getter
@@ -21,19 +17,7 @@ public class PageInfo {
   private final int totalPages;
   private final long totalElements;
   
-    /**
-     * Creates a `PageInfo` object containing information about the number of pages, page
-     * size, total pages, and total elements for a given `Pageable` and `Page`.
-     * 
-     * @param pageable pagination information for the page being processed, providing the
-     * page number, page size, total pages, and total elements.
-     * 
-     * @param page current page being processed, providing information on its position
-     * and size within the overall paginated dataset.
-     * 
-     * @returns a `PageInfo` object containing information about the current page of a
-     * paginated result.
-     */
+
     public static PageInfo of(Pageable pageable, Page<?> page) {
     return new PageInfo(
         pageable.getPageNumber(),
