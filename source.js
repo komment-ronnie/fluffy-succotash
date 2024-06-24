32ab35f4f7f1fab7b9d5b75f073d1f736af76d12
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-06-24 08:31:59 +0100

    Update source.js

--- source.js
+++ source.js
@@ -10,21 +10,12 @@ const a = () => {
 }
 
 
-/**
- * @description Simply logs "a" to the console and then exits the program with a
- * successful exit code of 0.
- */
-const c = () => {
+const b = () => {
   console.log("a");
   process.exit(0);
 
 }
 
-
-/**
- * @description Logs "a" to the console and exits the process with a status code of
- * 0.
- */
 const c = () => {
   console.log("a");
   process.exit(0);
