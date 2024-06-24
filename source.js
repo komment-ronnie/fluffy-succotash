9505fdfe7bfc4743a16848f7734c3d7c9a2cd50a
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-06-24 08:47:31 +0100

    Update source.js

--- source.js
+++ source.js
@@ -1,8 +1,4 @@
 
-/**
- * @description Logs "a" to the console and then terminates the script with a exit
- * code of 0.
- */
 const a = () => {
   console.log("a");
   process.exit(0);
@@ -10,10 +6,6 @@ const a = () => {
 }
 
 
-/**
- * @description Logs "a" to the console and then terminates the program with a success
- * status code of 0.
- */
 const b = () => {
   console.log("a");
   process.exit(0);
@@ -22,10 +14,6 @@ const b = () => {
 
 
 
-/**
- * @description Simply logs "a" to the console and then exits the program with a
- * status code of 0.
- */
 const c = () => {
   console.log("a");
   process.exit(0);
@@ -33,20 +21,12 @@ const c = () => {
 }
 
 
-/**
- * @description Outputs "a" to the console and then exits the program with a status
- * code of 0.
- */
 const d = () => {
   console.log("a");
   process.exit(0);
 
 }
 
-/**
- * @description Outputs "a" to the console and then exits the program with a status
- * code of 0.
- */
 const e = () => {
   console.log("a");
   process.exit(0);
@@ -54,20 +34,12 @@ const e = () => {
 }
 
 
-/**
- * @description Logs "a" to the console and exits the process with a status code of
- * 0.
- */
 const f = () => {
   console.log("a");
   process.exit(0);
 
 }
 
-/**
- * @description Logs "a" to the console and exits the process with a status code of
- * 0.
- */
 const g = () => {
   console.log("a");
   process.exit(0);
