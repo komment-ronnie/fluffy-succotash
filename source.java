045bc62261728a2d6acaf6329c0619ec69fadfad
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-06-24 09:30:04 +0100

    Update source.java

--- source.java
+++ source.java
@@ -6,12 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-/**
- * Is used to store and manipulate page and limit information for a pageable object.
- * It provides a compact representation of the current page number, page limit, total
- * pages, and total elements. The class offers utility methods for constructing
- * instances and generating new PageInfos based on pageable and page objects.
- */
+
 @EqualsAndHashCode
 @ToString
 @Getter
@@ -21,20 +16,7 @@ public class PageInfo {
   private final int pageLimit;
   private final int totalPages;
   private final long totalElements;
-  
-    /**
-     * Transforms a `Pageable` object and a `Page` object into a `PageInfo` object,
-     * containing information about the page number, page size, total pages, and total elements.
-     * 
-     * @param pageable pagination information for the page being processed, providing the
-     * page number, page size, total pages, and total elements.
-     * 
-     * @param page current page being processed, providing the total number of elements
-     * on that page.
-     * 
-     * @returns a `PageInfo` object containing information about the current page and
-     * total pages and elements.
-     */
+
     public static PageInfo of(Pageable pageable, Page<?> page) {
     return new PageInfo(
         pageable.getPageNumber(),
