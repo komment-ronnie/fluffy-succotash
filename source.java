3cf2bb13a428138f91772575ff0114b97e98b17c
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-06-26 15:48:22 +0100

    Update source.java

--- source.java
+++ source.java
@@ -6,12 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 @EqualsAndHashCode
 @ToString
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-  
 public class PageInfo {
   private final int currentPage;
   private final int pageLimit;
